804683b1c4d6cebf3e30d5681e8084c6fbe562b8
Tofique Ahmed Khan <tofiquekhan171819@gmail.com>
2024-03-20 15:15:45 +0530

    Initial Commit

---
+++ src/main/java/com/rentalstore/RentalStoreApplication.java
@@ -0,0 +1,13 @@
+package com.rentalstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rentalstore/RentalStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rentalstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
